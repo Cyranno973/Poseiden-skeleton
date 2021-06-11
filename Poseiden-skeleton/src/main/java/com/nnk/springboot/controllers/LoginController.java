052fcf68302c58e230df2bfce7d3a0f057932809
cyran <gs.gouin@gmail.com>
2021-06-11 21:26:07 +0200

    log

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.repositories.UserRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("app")
 public class LoginController {
+    private static final Logger logger = LogManager.getLogger("You are on the LoginController");
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("login")
     public ModelAndView login() {
+        logger.info("methode login : login");
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
         return mav;
@@ -23,6 +27,7 @@ public class LoginController {
 
     @GetMapping("secure/article-details")
     public ModelAndView getAllUserArticles() {
+        		logger.info("methode getAllUserArticles : secure/article-details");
         ModelAndView mav = new ModelAndView();
         mav.addObject("users", userRepository.findAll());
         mav.setViewName("user/list");
@@ -31,6 +36,7 @@ public class LoginController {
 
     @GetMapping("error")
     public ModelAndView error() {
+        		logger.info("methode error : error");
         ModelAndView mav = new ModelAndView();
         String errorMessage= "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -2,6 +2,9 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.Service.RatingService;
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.repositories.RatingRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,48 +18,67 @@ import javax.validation.Valid;
 
 @Controller
 public class RatingController {
+    private static final Logger logger = LogManager.getLogger("You are on the RatingController");
 
     private final RatingService ratingService;
+    private final RatingRepository ratingRepository;
 
     @Autowired
-    public RatingController(RatingService ratingService) {
+    public RatingController(RatingService ratingService, RatingRepository ratingRepository) {
         this.ratingService = ratingService;
+        this.ratingRepository = ratingRepository;
     }
 
     @RequestMapping("/rating/list")
     public String home(Model model) {
-        // TODO: find all Rating, add to model
+        logger.info("methode home : /rating/list");
         model.addAttribute("ratings", ratingService.allRating());
         return "rating/list";
     }
 
     @GetMapping("/rating/add")
     public String addRatingForm(Rating rating) {
+        logger.info("methode addRatingForm : /rating/add");
         return "rating/add";
     }
 
     @PostMapping("/rating/validate")
     public String validate(@Valid Rating rating, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Rating list
+        logger.info("methode validate : /rating/validate");
+        if (!result.hasErrors()) {
+            ratingRepository.save(rating);
+            model.addAttribute("curvePoint", rating);
+            return "redirect:/rating/list";
+        }
         return "rating/add";
     }
 
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Rating by Id and to model then show to the form
+        logger.info("methode showUpdateForm : /rating/update/{id}");
+        Rating rating = ratingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Rating ID" + id));
+        model.addAttribute("rating", rating);
         return "rating/update";
     }
 
     @PostMapping("/rating/update/{id}")
     public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
                                BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Rating and return Rating list
+        logger.info("methode updateRating : /rating/update/{id}");
+        if (result.hasErrors()) {
+            return "rating/update";
+        }
+        rating.setId(id);
+        ratingRepository.save(rating);
+        model.addAttribute("curvePoint", rating);
         return "redirect:/rating/list";
     }
 
     @GetMapping("/rating/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Rating by Id and delete the Rating, return to Rating list
+        logger.info("methode deleteRating : /rating/delete/{id}");
+        Rating rating = ratingRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Rating ID" + id));
+        ratingRepository.delete(rating);
         return "redirect:/rating/list";
     }
 }
