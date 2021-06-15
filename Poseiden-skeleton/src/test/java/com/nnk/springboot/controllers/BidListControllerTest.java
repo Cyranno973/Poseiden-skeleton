6b03ba56005e77335f5835d702d57c40b5081564
cyran <gs.gouin@gmail.com>
2021-06-15 21:20:13 +0200

    test

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/BidListControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/BidListControllerTest.java
@@ -1,17 +1,30 @@
 package com.nnk.springboot.controllers;
 
-import junit.framework.TestCase;
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-public class BidListControllerTest extends TestCase {
-//    @Autowired
-//    BidListController bidListController;
-//
-//    @Test
-//    public void testHome() {
-//        String result = bidListController.home();
-//        Assertions.assertThat(result).isEqualTo("login");
-//    }
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+public class BidListControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username ="e1" , password = "e1")
+    public void home() throws Exception {
+        mockMvc.perform(get("/bidList/list"))
+                .andExpect(redirectedUrl("http://localhost/login"));
+
+    }
 }
